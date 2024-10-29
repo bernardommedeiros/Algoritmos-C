845dc9f846b62b91eec0be49f08778fc3455f53a
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-10-29 14:20:14 +0000

    array

--- Aulas/aula04/array.c
+++ Aulas/aula04/array.c
@@ -1,3 +1,6 @@
+// A array armazena uma coleção de elementos do mesmo tipo,  acessados pelo indice
+// A declaração de um array em C é feita da seguinte forma: tipo nomeVar[tamanho];
+
 #include <stdio.h>
 int main (int argc, char **argv){
 	int i, x[10];
