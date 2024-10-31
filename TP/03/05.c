e99a32980411e04e5a38a08fa1835613feebd109
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-10-31 13:30:22 +0000

    06.c

---
+++ TP/03/05.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    float num[100];
+    printf("Digite a sequencia de numeros reais:")
+
+    
+}
---
+++ TP/03/06.c
@@ -0,0 +1,18 @@
+#inlcude <stdio.h>
+int main (){  
+
+    int saida;
+    int n;
+    int k;
+    float nota;
+
+    saida = 100;
+    do {
+        print ("Pesos das provas:");
+        scanf ("%d", &k);
+        print ("Nota do aluno:");
+        scanf ("%d", &nota);
+} while (not(saida));
+
+return 0
+}
